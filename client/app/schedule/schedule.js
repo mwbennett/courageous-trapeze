931aa3bdbd31304e405609f64e837f24d182f277
Mark Bennett <mwbennett@wesleyan.edu>
2015-05-12 19:03:43 -0700

    deleted debugger statement

--- client/app/schedule/schedule.js
+++ client/app/schedule/schedule.js
@@ -28,7 +28,6 @@ schedule.controller('ScheduleController', ['$scope', 'Messages', 'Contacts', fun
   
   $scope.addMessage = function (message) {
     $scope.loading = true;
-    debugger;
     Messages.addMessage(message)
       .success(function () {
         $scope.message.contactId = '';
