d34c044e67cb58f78b03a0c2ecbfb53036b52267
Mark Bennett <mwbennett@wesleyan.edu>
2015-05-14 13:52:51 -0700

    [ feat ] messages can now include emoji shortnames, which render on the 'messages' page as emojis

--- client/app/messages/messages.js
+++ client/app/messages/messages.js
@@ -9,7 +9,10 @@ angular.module('courageousTrapeze.messages', [])
     $scope.loading = true;
     Messages.fetch()
       .then(function (messages) {
-        console.log(messages);
+        // convert emoji shortnames to images, and create an html element to render on the view 
+        messages.forEach(function(message){
+          message.htmlText = '<p>' + emojione.shortnameToImage(message.text) + '</p>';
+        });
         $scope.messages = messages;
       })
       .catch(function (error) {
@@ -21,4 +24,25 @@ angular.module('courageousTrapeze.messages', [])
   };
 
   $scope.getAllMessages();
+
+}])
+
+// custom directive to render the html message on the view 
+.directive("bindCompiledHtml", ['$compile', '$timeout', function ($compile, $timeout) {
+  return {
+    template: '<div></div>',
+    scope: {
+      rawHtml: '=bindCompiledHtml'
+    },
+    link: function(scope, elem, attrs) {
+      scope.$watch('rawHtml', function(value) {
+        if (!value) return;
+        // we want to use the scope OUTSIDE of this directive
+        // (which itself is an isolate scope).
+        var newElem = $compile(value)(scope.$parent);
+        elem.contents().remove();
+        elem.append(newElem);
+      });
+    }
+  };
 }]);
